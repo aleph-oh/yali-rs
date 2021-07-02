867314c466199c11fe8a8e2cc489161277190ddb
aleph-oh <jhilton@mit.edu>
2021-07-02 19:18:24 -0400

    Add tests in external files for lexer

--- src/lexer.rs
+++ src/lexer.rs
@@ -13,14 +13,42 @@ pub(crate) fn lex<S: AsRef<str>>(src: S) -> Vec<String> {
 #[cfg(test)]
 mod tests {
     use crate::lexer::lex;
+    use serde::{Deserialize, Serialize};
+    use serde_json;
+    use std::fs::File;
+    use std::path::Path;
+
+    #[derive(Serialize, Deserialize)]
+    struct TestData {
+        input: String,
+        expected: Vec<String>
+    }
+
+    fn check<P: AsRef<Path>>(file: P) {
+        let test_file = File::open(file).expect("Failed to open test data file");
+        let test_data: Vec<TestData> = serde_json::from_reader(test_file).expect("Could not parse test data file");
+        for example in test_data {
+            assert_eq!(lex(example.input), example.expected);
+        }
+    }
 
     #[test]
-    fn lex_atom() {
+    fn atom() {
         assert_eq!(lex("1"), vec!["1"])
     }
 
     #[test]
-    fn lex_s_expression() {
-        assert_eq!(lex("(+ 1 1)"), vec!["(", "+", "1", "1", ")"])
+    fn s_expression() {
+        assert_eq!(vec!["(", "+", "1", "1", ")"], lex("(+ 1 1)"))
+    }
+
+    #[test]
+    fn multiline() {
+        check("./lex_examples/multiline.json")
+    }
+
+    #[test]
+    fn comments() {
+        check("./lex_examples/comments.json")
     }
 }
