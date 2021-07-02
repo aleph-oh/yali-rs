abb76269c552c2cc51895d462cddccde3b2e27f4
aleph-oh <jhilton@mit.edu>
2021-07-02 18:15:35 -0400

    Implement lexer

--- src/lexer.rs
+++ src/lexer.rs
@@ -1,7 +1,13 @@
 //! This module contains a lexer / tokenizer which supports single-line comments.
 
-fn lex(src: &str) -> Vec<&str> {
-    unimplemented!()
+/// Lexes, or tokenizes, a provided string into individual separated tokens.
+pub(crate) fn lex<S: AsRef<str>>(src: S) -> Vec<String> {
+    let src = src.as_ref();
+    src.replace("(", " ( ")
+        .replace(")", " ) ")
+        .split_whitespace()
+        .map(&str::to_string)
+        .collect()
 }
 
 #[cfg(test)]
@@ -18,4 +24,3 @@ mod tests {
         assert_eq!(lex("(+ 1 1)"), vec!["(", "+", "1", "1", ")"])
     }
 }
-
